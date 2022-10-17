b9aecad7d3fc38f8a41478e46bbfd9c1e0654f63
ryn-crypto <riyanfirst13@gmail.com>
2022-10-17 19:59:37 +0700

    menggunakan vigenesia untuk membuat project flutter

---
+++ lib/Constant/Const.dart
@@ -0,0 +1,1 @@
+String url = "http://10.0.2.2";
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'screen/login.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    ));
